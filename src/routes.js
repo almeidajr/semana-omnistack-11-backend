8e53c991b4ebf3ed041bdab2d6a9876fc15b7153
José Rogério de Almeida Júnior <almeidajr.vr@gmail.com>
2020-03-24 11:01:23 -0300

    Aula 02

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const NgoController = require('./controllers/NgoController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/session', SessionController.create);
+
+routes.post('/ngos', NgoController.create);
+routes.get('/ngos', NgoController.index);
+
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+module.exports = routes;
